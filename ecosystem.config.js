45106a7f301826ea9a6a21fb1493c99dbfdd60ac
komiwalnut <62009152+komiwalnut@users.noreply.github.com>
2025-02-03 12:43:21 +0800

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      name: "ronin-wallet-tracker",
+      script: "main.py",
+      interpreter: "rwt-venv/bin/python3",
+      version: "1.0.0"
+    }
+  ]
+};
